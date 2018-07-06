bc7bfbc08c413d05d1d2b5ca08d4bc04d7be80e3
geraldjglasgow <geraldjglasgow@gmail.com>
2018-07-06 04:07:46 +0000

    changing to sqlite3:

--- commands/general/postCount.js
+++ commands/general/postCount.js
@@ -14,20 +14,7 @@ class PostCounter extends commando.Command {
 
     // code run when command is called
     async run(message, args) {
-        var user = message.author.id;
-        var url = 'mongodb://localhost:27017';
-        var MongoClient = mongodb.MongoClient;
-        MongoClient.connect(url,(err,database) =>{
-            const db = database.db('discord');
-            var coll = db.collection('user_info');
-            coll.find({id: user}).toArray(function (err, result) {
-                if(result.length) {
-                    message.channel.send(message.author.username + ' has sent ' + result[0].post_count + ' messages.');
-                } else {
-                    message.send('No data found');
-                }
-            })
-        });
+
     }
 }
 
--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 // settings up library for discord bot
-const mongodb = require('mongodb');
+const sqlite3 = require('sqlite3').verbose();
 const commando = require('discord.js-commando');
-const token = 'NDIwNjUxODE1MjgyMDE2MjU2.DYHaIQ.s4poWTyrhxMtc0QB0R7cU-yBqsg';
-const bot = new commando.Client();
+const assert = require('assert');
+const bot = new commando.Client({
+	unknownCommandResponse: false
+});
+
+
 
 // collection: champions, {name: 'Brand'}, {role: 'Mid'}, {role2: 'Support'}, {type: 'AP'}
 
-
 // commands are split into groups
-bot.registry.registerGroup('league', 'League');
-bot.registry.registerGroup('general', 'General');
+//bot.registry.registerGroup('league', 'League');
+//bot.registry.registerGroup('general', 'General');
 //bot.registry.registerDefaults();
-bot.registry.registerCommandsIn(__dirname + "/commands")
+//bot.registry.registerCommandsIn(__dirname + "/commands")
 bot.login(token);
 
 bot.on('message', message => {
@@ -33,12 +36,14 @@ bot.on('message', message => {
     }
 
     // accessing mongodb to update post count
-    var url = 'mongodb://localhost:27017';
-    var MongoClient = mongodb.MongoClient;
-    MongoClient.connect(url,(err,database) =>{
-        const db = database.db('discord');
-        var coll = db.collection('userInfo');
-        coll.updateOne({'id':user_id}, {$set: {id:user_id}, $inc:{post_count:1}}, {upsert: true});
-        database.close();
-    });
+	let update = "UPDATE user_data SET post_count = post_count + 1 WHERE id = " + user_id;
+	let insert = "INSERT OR IGNORE INTO user_data(id) values(" + user_id + ")";
+	let db = new sqlite3.Database('discord.db', (err) => {
+		if(err){
+			return console.error(err.message);
+		}
+		db.run(insert);
+		db.run(update);
+		db.close();
+	});
 });
